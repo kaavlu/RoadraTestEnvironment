deb58f87e529c03bfad1c58f2e426ef1c8254998
Koldewarrior44 <bridgerkolde@gmail.com>
2023-06-23 14:09:40 -0400

    fix

--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -22,13 +22,8 @@ const actions = {
 };
 
 const mutations = {
-<<<<<<< HEAD
   // addPageLink: (state, value) => {
   //   state.homePageLinks.push(value);
-=======
-  // toggleErrorModal: (state, value) => {
-  //   state.errorModal = value;
->>>>>>> 89fb234ff6b6b085dbd5be4e5ef27a1fcf6e1de5
   // },
 };
 
