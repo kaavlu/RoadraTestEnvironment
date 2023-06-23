ce5697aebfcf13ce2783003a421248a1b8c27f8e
Koldewarrior44 <bridgerkolde@gmail.com>
2023-06-23 14:30:44 -0400

    modal ability

---
+++ src/components/header/Modals.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <GeneralModal v-if="$store.getters.generalModal" />
+    <!-- <SideModal v-if="$store.getters.sideModal" />
+    <FullscreenLoader v-if="$store.getters.fullScreenLoader" /> -->
+  </div>
+</template>
+
+<script>
+import GeneralModal from "@/components/general/modals/GeneralModal.vue"
+export default {
+  name: "Modals",
+  components: {
+    GeneralModal
+  },
+  props: {
+    route: String,
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/home/HomeWelcomeModal.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>
+
+    </h1>
+    <p>
+      
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeWelcomeModal"
+}
+</script>
+
+<style scoped>
+
+</style>
